7f19f583ecc8400cdfb9c3967171fd37a81bc429
gabriella kovacs <kovacsxgabriella@gmail.com>
2016-02-07 00:53:35 +0000

    modify recommendation section, utilize masonry library to arrange images

--- js/removeCarouselIndicators.js
+++ js/removeCarouselIndicators.js
@@ -43,3 +43,13 @@ $("#backToFirst").click(function(){
     changeDescription();
     checkitem();
 });
+
+$('.grid').imagesLoaded( function() {
+    $('.grid').masonry({
+        // set itemSelector so .grid-sizer is not used in layout
+        itemSelector: '.grid-item',
+        // use element for option
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    })
+});
