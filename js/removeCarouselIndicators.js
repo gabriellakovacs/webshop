3c7ed3759e21e3d9df0fe16c1f0b2d6236aad694
gabriella kovacs <kovacsxgabriella@gmail.com>
2016-02-08 12:23:37 +0000

    concatenate js files and modularize carousel navigation customizing functions

--- js/removeCarouselIndicators.js
+++ js/removeCarouselIndicators.js
@@ -1,52 +1,90 @@
-$('#productCarousel').carousel({
-    interval: false,
-})
+$(document).ready(function () {  
 
-$(document).ready(function () {             
-    checkitem();
-});
+    var colorOptionList = document. querySelectorAll(".chooseColor label"); 
+    var showcaseProductsCarousel =  $('#productCarousel');   
 
-$('#productCarousel').on('slid.bs.carousel', checkitem);
-$('#productCarousel').on('slid.bs.carousel', changeDescription);
+    setBackgroundColorForColorOptions();
 
-function checkitem() {
-    var $this = $('#productCarousel');
-    if ($('.carousel-inner .item:first').hasClass('active')) {
-        $this.children('.left.carousel-control').hide();
-        $this.children('.right.carousel-control').hide();
-        $('.carousel-indicators .active').removeClass('active');
-        $('.carousel-indicators li:first').addClass('active');
-        $('.carousel-indicators').show();
-        $("#backToFirst").css('visibility', 'hidden');
-    } else {
-        $('.carousel-indicators').hide();
-        $this.children('.carousel-control').show();
-        $("#backToFirst").css('visibility', 'visible');
-        $this.children('.carouselArrowContainer.right').prop('disabled', false);
-        $this.children('.carouselArrowContainer.right').removeClass('disabled');
+    customizeCarouselNavigation();
 
+    showcaseProductsCarousel.on('slid.bs.carousel', customizeCarouselNavigation);
+    showcaseProductsCarousel.on('slid.bs.carousel', changeDescription);
+
+    showcaseProductsCarousel.carousel({
+        interval: 10000,
+    })
+
+    function customizeCarouselNavigation() {
+        carouselBackToFirstSlideButton();
+        carouselDisableRightArrowsOnLastSlide();
+        carouselRemoveArrowsOnFirstSlide();
+        carouselIndicatorsOnlyOnFirstSlide();
+    }
+
+    function changeDescription() {
+        var activeImgId = $('#productCarousel .carousel-inner .active').attr('id');
+        $(".productName.active").removeClass('active');
+        $("." + activeImgId).addClass('active');
+    }
+
+    function setBackgroundColorForColorOptions() {
+        for (var l = 0; l < colorOptionList.length; l++) {
+            var color = (colorOptionList[l].htmlFor).split("-")[1];
+            colorOptionList[l].style.background = "#" + color;
+        }
+    }
+
+    function carouselIndicatorsOnlyOnFirstSlide() {
+        var $this = $('#productCarousel');
+        if ($('.carousel-inner .item:first').hasClass('active')) {
+            $('.carousel-indicators').show();
+        } else {
+            $('.carousel-indicators').hide();
+        }
+    }
+
+    function carouselRemoveArrowsOnFirstSlide() {
+        var $this = $('#productCarousel');
+        if ($('.carousel-inner .item:first').hasClass('active')) {
+            $this.children('.carousel-control').hide();
+        } else {
+            $this.children('.carousel-control').show();
+        }
+    }
+
+    function carouselDisableRightArrowsOnLastSlide() {
+        var $this = $('#productCarousel');
         if ($('.carousel-inner .item:last').hasClass('active')) {
             $this.children('.carouselArrowContainer.right').prop('disabled', true);
             $this.children('.carouselArrowContainer.right').addClass('disabled');
-        
+        } else {
+            $this.children('.carouselArrowContainer.right').prop('disabled', false);
+            $this.children('.carouselArrowContainer.right').removeClass('disabled');
         }
     }
-}
 
-function changeDescription() {
- var activeImgId = $('#productCarousel .carousel-inner .active').attr('id');
- $(".productName.active").removeClass('active');
- $("." + activeImgId).addClass('active');
-}
 
-$("#backToFirst").click(function(){
-    $('#productCarousel .carousel-inner .active').removeClass('active');
-    $('#productCarousel .carousel-inner .item:first').addClass('active');
-    changeDescription();
-    checkitem();
+    function carouselBackToFirstSlideButton() {
+        var $this = $('#productCarousel');
+        if ($('.carousel-inner .item:first').hasClass('active')) {
+             $("#backToFirst").css('visibility', 'hidden');
+        } else {
+            $("#backToFirst").css('visibility', 'visible');
+        }
+    }
+
+    $("#backToFirst").click(function(){
+        $('#productCarousel .carousel-inner .active').removeClass('active');
+        $('#productCarousel .carousel-inner .item:first').addClass('active');
+        $('.carousel-indicators .active').removeClass('active');
+        $('.carousel-indicators li:first').addClass('active');
+        changeDescription();
+        customizeCarouselNavigation();
+    });
+
 });
 
-$('.grid').imagesLoaded( function() {
+window.onload = function() {
     $('.grid').masonry({
         // set itemSelector so .grid-sizer is not used in layout
         itemSelector: '.grid-item',
@@ -54,4 +92,10 @@ $('.grid').imagesLoaded( function() {
         columnWidth: '.grid-sizer',
         percentPosition: true
     })
-});
+           
+}
+
+
+
+
+
